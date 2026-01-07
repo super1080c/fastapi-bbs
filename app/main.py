2d8ad0a153e524db03f4b91fbccc6076b15bdcf5
super1080c <ckg24026@st.ckg.ac.jp>
2026-01-07 23:16:15 +0900

    feature: 投稿(Post)APIとスキーマの追加

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routers import threads
+from app.routers import posts
 
 app = FastAPI(
     title="BBS API",
@@ -9,3 +10,5 @@ app = FastAPI(
 
 # ルーターを登録（まだ中身は空でOK）
 app.include_router(threads.router)
+app.include_router(posts.router)
+app.include_router(posts.threads_router)
